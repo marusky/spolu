c0d410c626d31ba80c7748c6bf6ffe6bea86d176
marusky <adrianmarusakza@gmail.com>
2021-11-06 12:44:51 +0100

    Configure devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
 end
