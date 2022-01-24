05e5979be2c2dd6be2baaa7b375b36fe0455efc1
marusky <adrianmarusakza@gmail.com>
2022-01-24 11:04:23 +0100

    Add styling & fix turbolinks load

--- app/javascript/packs/teams.js
+++ app/javascript/packs/teams.js
@@ -1,16 +1,16 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbolinks:load', () => {
 
     const teamsContainer = document.querySelector('.teams-container')
     const width = teamsContainer.getBoundingClientRect().width
 
     const btnChevron = document.querySelector('.btn-chevron')
     const rowsCount = Math.ceil(teamsContainer.children.length / 3)
-    teamsContainer.style.height = `${window.innerWidth / 4 + 17}px`;
+    teamsContainer.style.height = `${window.innerWidth / 4 + 19}px`;
 
     btnChevron.addEventListener('click', () => {
         if ([...btnChevron.classList].includes('open')) {
             btnChevron.classList.remove('open')
-            teamsContainer.style.height = `${window.innerWidth / 4 + 17}px`;
+            teamsContainer.style.height = `${window.innerWidth / 4 + 19}px`;
             teamsContainer.style.marginBottom = '20px'
         } else {
             btnChevron.classList.add('open')
